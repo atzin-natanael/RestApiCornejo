9ccfc01b240779745e860a4621a0bb0212e2f2ba
npacheco <atzin_pacheco@live.com>
2026-03-29 14:35:36 -0600

    up cornejo7

--- routes/inventarioRoutes.js
+++ routes/inventarioRoutes.js
@@ -3,7 +3,7 @@ const codigosController = require('../controllers/appController.js')
 const router = express.Router()
 
 router.post('/guardarRegistro',codigosController.guardarArticuloInventario)
-router.post('/inventario/eliminar-articulo/:id', codigosController.eliminarRegistro);
+router.post('/eliminar-articulo/:id', codigosController.eliminarRegistro);
 router.get('/mostrarTabla/:id', codigosController.mostrarArticulosInventario)
 router.get('/colectores', codigosController.mostrarColectores)
 router.get('/colectores/:id', codigosController.mostrarColectoresById)
