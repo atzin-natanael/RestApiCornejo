209da499a85ab3fe0cf1ab63f4f3eedd2be41511
npacheco <atzin_pacheco@live.com>
2026-05-10 11:44:43 -0600

    ip

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const whitelist = [
     'http://localhost:3001', 
     'http://localhost:8000', 
     'https://paginainventariofisico.onrender.com',
-     'https://pedidos.isioffice.com/'// Agrega tu URL de producción aquí
+     'https://pedidos.isioffice.com'// Agrega tu URL de producción aquí
 ];
 
 app.use(cors({
